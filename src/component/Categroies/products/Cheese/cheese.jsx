a1cf6ee4a0248c3f78e0d3d1aa78874a682e0194
Mohamed Fawzy <mohamed.elsakaa015@gmail.com>
2024-07-08 16:03:44 +0300

    update

--- src/component/Categroies/products/Cheese/cheese.jsx
+++ src/component/Categroies/products/Cheese/cheese.jsx
@@ -18,35 +18,20 @@ function Cheese() {
         <div className="cheese-ingre">
           <h1>Lion Cheese</h1>
           <p>
-            City Farm offers the finest quality Colcasia grown in the rich
-            fertile soil of Egypt. It is carefully selected, cut, washed and
-            frozen. Our Colcasia is ready for cooking as each pack contains a
-            small bag of green leaves, essential for the flavor, to make
-            preparing quick and easy.
+            Fresh Potato slices fried in oil This product contains no
+            preservatives.
           </p>
           <div className="cheese-data">
             <h3>Ingredients:</h3>
-            <p>
-              Garden fresh frozen Colcas (Taro Roots) and spices. No additives.
-            </p>
+            <p>Fresh Potatoes - Vegetables - Oil - Cheese Flavor</p>
             <h3>Store</h3>
             <p className="cheese-store">
-              City Farm Colcas and spices stay fresh up to 24 hours when stored
-              in a cold place or up to expiry date when stored at -18°C.
-              <span>(Shelif-Store)</span>
+              <span>(Shelf-Stable)</span>
             </p>
-            <h3>Cooking Suggestion:</h3>
+            <h3>Food Allergy:</h3>
             <p>
-              To retain maximum flavor don&apos;t thaw before cooking. Drop in
-              boiling water and cook over medium heat for 8-10 minutes, drain
-              and add butter.
-            </p>
-            <h3>Other Cooking Suggestion:</h3>
-            <p>
-              Other Cooking Suggestions: Drop frozen colcas into 4 boiling
-              bouillon broth. Add salt, pepper, cook for 4 minutes adding the
-              included spices. Cook some garlic in 1 tbsp butter. Add to
-              mixture. Serve over cooked rice.
+              Gluten and product thereof - Wheat and product thereof - Milk and
+              dairy product thereof.
             </p>
           </div>
         </div>
